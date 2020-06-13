05544fd8f340a70c815bbe0266d099494b5484f9
Hep <hepei@shulie.io>
2020-06-13 13:31:15 +0800

    T

--- deploy.sh
+++ deploy.sh
@@ -18,9 +18,11 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-git push -f git@github.com:he-7356/he-7356.github.io.git master
+#git push -f git@github.com:he-7356/he-7356.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f https://github.com/he-7356/myBlog.git master:gh-pages
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+ git push -f git@github.com:he-7356/myBlog.git master:gh-pages
 
 cd -
